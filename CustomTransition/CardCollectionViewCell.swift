475ccb69952529d24c68e9c1bb3116406419bc83
Hunter Maximillion Monk <hunter.monk@gmail.com>
2016-08-02 00:41:25 -0500

    Initial commit, add pet description label

---
+++ CustomTransition/CardCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CardCollectionViewCell.swift
+//  CustomTransition
+//
+//  Created by Hunter Monk on 8/2/16.
+//  Copyright © 2016 Hunter Monk. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
